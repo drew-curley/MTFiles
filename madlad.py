6c5d88c1bc2c2b186135b38dcaee42c6cc2cf581
Drew Curley <drew_curley@wycliffeassociates.org>
2024-07-23 13:57:16 -0400

    updates

---
+++ madlad.py
@@ -0,0 +1,50 @@
+# run off of GPU in PyTorch
+import torch
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+
+# check if cuda is available
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("Using GPU:", torch.cuda.get_device_name(0))
+else:
+    device = torch.device("cpu")
+    print("CUDA is not available, using CPU instead.")
+
+######
+
+# # Load the pipeline and specify the device (0 for the first GPU)
+# classifier = pipeline("sentiment-analysis", device=0)
+
+# # Perform inference
+# result = classifier("I love using the new GPU for computations!")
+# print(result)
+
+# repo = "facebook/nllb-200-3.3B"
+repo = "google/madlad400-3b-mt"
+
+
+model = AutoModelForSeq2SeqLM.from_pretrained(repo)
+model = model.to(device)
+
+tokenizer = AutoTokenizer.from_pretrained(repo)
+
+translator = pipeline('translation', model=model, tokenizer=tokenizer, src_lang="eng_Latn", tgt_lang='npi_Deva', max_length = 1000000, device=0)
+ 
+x = """
+The gospel of John has 21 chapters. This plan will help you read all of them in 24 days. Each day, read the Scripture and observe what it says. Answer the questions. Then think about the passage, what you have learned, and how you can apply it to your life. Use the checkboxes to keep track of your progress. 
+Day 1
+☐  Read John 1:1-5
+•	What do you learn about God? 
+☐  Read John 1:6-13
+•	What does a witness do? 
+•	How can you be a good witness?
+•	Describe the true Light.
+
+
+
+”"""
+
+
+for line in x.splitlines():
+    print(translator(line)[0]["translation_text"])
+               
