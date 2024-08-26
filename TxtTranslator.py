1aeb2b2b917726f866bff799c1cd7423ff854eb8
Darrell Cole Hill <darrellcolehill@gmail.com>
2024-08-26 16:12:14 -0400

    Updated TxtTranslator
    Still need to add src lang check

--- TxtTranslator.py
+++ TxtTranslator.py
@@ -6,12 +6,11 @@ import torch
 from collections import Counter
 from pathlib import Path
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline, TRANSFORMERS_CACHE
-from nltk.tokenize import sent_tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize
 from zipfile import BadZipFile
 import nltk
 import json
 from TranslatorInterface import TranslatorInterface
-
 
 nltk.download('punkt')
 nltk.download('punkt_tab')
@@ -19,93 +18,37 @@ nltk.download('punkt_tab')
 class TxtTranslator(TranslatorInterface):
 
     def __init__(self, pretrained_lang_model="./lid218e.bin"):
+        self.chunk_word_count=200
         self.ext_in = "txt"
         self.ext_out = "txt"
         self.pretrained_lang_model = pretrained_lang_model
 
         with open("./constants/model_checkpoints.json", 'r') as json_file:
             self.checkpoints = json.load(json_file)
-            # "NLLB": "facebook/nllb-200-3.3B",
-            # "MADLAD": "google/madlad400-3b-mt",
-            # "Llama-3.1-405B": "meta-llama/Meta-Llama-3.1-405B"
 
         with open("./constants/languages.json", 'r') as json_file:
             self.languages = json.load(json_file)
-                # "Ethiopian": "amh_Ethi",
-                # "Arabic": "arb_Arab",
-                # "Assamese": "asm_Beng",
-                # "Bangal": "ben_Beng",
-                # "BPortugese": "por_Latn",
-                # "Burmese": "mya_Mymr",
-                # "Cebuano": "ceb_Latn",
-                # "Chinese": "zsm_Latn",
-                # "French": "fra_Latn",
-                # "Gujarati": "guj_Gujr",
-                # "Hausa": "hau_Latn",
-                # "Hindi": "hin_Deva",
-                # "Illocano": "ilo_Latn",
-                # "Indonesian": "ind_Latn",
-                # "Kannada": "kan_Knda",
-                # "Khmer": "khm_Khmr",
-                # "Laotian": "lao_Laoo",
-                # "LASpanish": "spa_Latn",
-                # "Malayalam": "mal_Mlym",
-                # "Nepali": "npi_Deva",
-                # "Oriya": "ory_Orya",
-                # "PlatMalagasy": "plt_Latn",
-                # "EPunjabi": "pan_Guru",
-                # "Russian": "rus_Cyrl",
-                # "Swahili": "swh_Latn",
-                # "Tagalog": "tgl_Latn",
-                # "Tamil": "tam_Taml",
-                # "Telugu": "tel_Telu",
-                # "Thai": "tha_Thai",
-                # "TokPisin": "tpi_Latn",
-                # "Urdu": "urd_Arab",
 
         self._prepare_language_detection_model()
 
-
-    def _prepare_language_detection_model(self):
-        if not os.path.isfile(self.pretrained_lang_model):
-            # Download the model if it doesn't exist
-            os.system(f"wget https://dl.fbaipublicfiles.com/nllb/lid/lid218e.bin")
-
-
-    def load_model(self, model_name):
-        model_dir = f"{TRANSFORMERS_CACHE}/{model_name}"
-
-        if not os.path.exists(model_dir):
-            print(f"{model_name} not found")
-            tokenizer = AutoTokenizer.from_pretrained(model_name)
-            model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-            model.save_pretrained(model_dir)
-            tokenizer.save_pretrained(model_dir)
-        else:
-            print(f"{model_name} found")
-            tokenizer = AutoTokenizer.from_pretrained(f"{model_dir}")
-            model = AutoModelForSeq2SeqLM.from_pretrained(f"{model_dir}")
-
-        return model, tokenizer
-
-
-    def unload_model(self, model, tokenizer):
-        del model
-        del tokenizer
-        gc.collect()
-        torch.cuda.empty_cache()
-
-
-    def get_languages(self, file):
+    def _get_languages(self, file):
         print("TODO")
+        return True
         # TODO: finish
 
-    def translate(self, text, source_language, target_language):
-        # TODO: account for this being a .txt file
-        """Translate a given text from src_lang to tgt_lang using the specified model."""
-        # TODO: allow the translate method to specify what model to use. Possibly make an Enum class from 
-        #  the languages.json file. 
-        model, tokenizer = self.load_model(self.checkpoints["NLLB-distilled"])
+    def translate(self, filePath, source_language, target_language, model_name):
+
+        # TODO: add src lang check here.
+
+        if target_language not in self.languages:
+            print(f"Cannot translate. {target_language} is not a supported target language.")
+            return
+        
+        if model_name not in self.checkpoints:
+            print(f"Cannot translate. {model_name} is not a supported model.")
+            return 
+
+        model, tokenizer = self._load_model(self.checkpoints[model_name])
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         translation_pipeline = pipeline('translation',
@@ -116,13 +59,54 @@ class TxtTranslator(TranslatorInterface):
                                         max_length=400,
                                         device=device)
 
-        translated_text = translation_pipeline(text)[0]['translation_text']
+        translated_chunks = []
 
-        self.unload_model(model, tokenizer)
-        return translated_text
+        with open(filePath, 'r', encoding='utf-8') as file:
+            text = file.read()
+            words = word_tokenize(text)
+            
+            current_chunk = []
+            current_word_count = 0
+
+            for word in words:
+                current_chunk.append(word)
+                current_word_count += 1
+
+                if current_word_count >= self.chunk_word_count:
+                    chunk_text = ' '.join(current_chunk)
+                    sentences = sent_tokenize(chunk_text)
+                    for sentence in sentences:
+                        translated_sentence = translation_pipeline(sentence)[0]['translation_text']
+                        translated_chunks.append(translated_sentence)
+                    current_chunk = []
+                    current_word_count = 0
+            
+            # Handle any remaining words in the last chunk
+            if current_chunk:
+                chunk_text = ' '.join(current_chunk)
+                sentences = sent_tokenize(chunk_text)
+                for sentence in sentences:
+                    translated_sentence = translation_pipeline(sentence)[0]['translation_text']
+                    translated_chunks.append(translated_sentence)
+
+        self._unload_model(model, tokenizer)
+
+        # Frees the pipeline
+        del translation_pipeline
+        gc.collect()
+        torch.cuda.empty_cache()   
+
+        output_file_path = filePath.with_name(f"{filePath.stem}_translated.{self.ext_out}")
+        with open(output_file_path, 'w', encoding='utf-8') as out_f:
+            for translated_chunk in translated_chunks:
+                out_f.write(translated_chunk + "\n")
+
+        print(f"Translation saved to {output_file_path}")
+        return output_file_path
+
 
 
 # Example usage:
 # translator = TxtTranslator()
-# translated_text = translator.translate("Hello, world!", "eng_Latn", "fra_Latn")
-# print(translated_text)
+# file_path = Path("/mnt/c/Users/hilld/Documents/Github/MTFiles/Input/test.txt")
+# translated_text = translator.translate(file_path, "eng_Latn", "fra_Latn", "NLLB-distilled")
